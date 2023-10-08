f1c87d226061a4f00ec422ce3c4918020b0f0a8d
Nataliia Diekmann <nataliia.diekmann@gmail.com>
2023-10-08 01:07:42 -0500

    Added codewars

---
+++ 8kyu/Basic_variable_assignment.java
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//        This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+//
+//        DEBUGGING
+public class Basic_variable_assignment {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
