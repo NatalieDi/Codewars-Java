e4a51adf9c62f1177b07ad3c8182189900b483f5
Nataliia Diekmann <nataliia.diekmann@gmail.com>
2023-10-08 11:42:33 -0500

    Added original task

--- 8kyu/Basic_variable_assignment.java
+++ 8kyu/Basic_variable_assignment.java
@@ -1,6 +1,10 @@
 //DESCRIPTION:
 //        This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
-//
+// public class Kata {
+//   public static String a == "code";
+//   public static String b == "wa.rs";
+//   public static String name == a + b;
+// }
 //        DEBUGGING
 public class Basic_variable_assignment {
     public static String a = "code";
