a597b6c099b82fca70fe9041f86ebc7eed957f1f
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-24 18:24:29 -0500

    Highest and Lowest, Exes and Ohs

---
+++ 7kyu/HighestandLowest.java
@@ -0,0 +1,36 @@
+/*https://www.codewars.com/kata/554b4ac871d6813a03000035/java
+DESCRIPTION:
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5")  // return "5 1"
+highAndLow("1 2 -3 4 5") // return "5 -3"
+highAndLow("1 9 3 4 -5") // return "9 -5"
+ */
+public class HighestandLowest {
+    public static void main(String[] args) {
+        System.out.println(highAndLow("1 2 3 4 5"));// "5 1"
+        System.out.println(highAndLow("1 2 -3 4 5"));// "5 -3"
+        System.out.println(highAndLow("1 9 3 4 -5"));// "9 -5"
+    }
+    public static String highAndLow(String numbers) {
+        String[] numbersArray = numbers.split(" ");
+        String max = numbersArray[0];
+        String min = numbersArray[0];
+
+        for (int i = 0; i < numbersArray.length; i++) {
+            if (Integer.parseInt(max) <= Integer.parseInt(numbersArray[i])) {
+                max = numbersArray[i];
+
+            }
+            if (Integer.parseInt(min) >= Integer.parseInt(numbersArray[i])) {
+                min = numbersArray[i];
+
+
+            }
+        }
+        return (max + " " + min);
+
+    }
+
+}
