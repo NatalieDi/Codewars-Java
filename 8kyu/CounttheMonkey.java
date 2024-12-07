84073c9fffa214c2e559b5b98bf8875b2e81a3ab
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-07 12:20:45 -0600

    updated monkey count to streams

--- 8kyu/CounttheMonkey.java
+++ 8kyu/CounttheMonkey.java
@@ -11,6 +11,7 @@ For example(Input --> Output):
 */
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class CounttheMonkey {
     public static void main(String[] args) {
@@ -18,13 +19,19 @@ public class CounttheMonkey {
         System.out.println(Arrays.toString(monkeyCount(1)));
     }
 
-    public static int[] monkeyCount(final int n) {
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        return arr;
-    }
+//    public static int[] monkeyCount(final int n) {
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = i + 1;
+//        }
+//        return arr;
+//    }
 
+
+    public static int[] monkeyCount(final int n) {
+        return IntStream.rangeClosed(1, n).toArray();
+    }
 }
+
+
 
