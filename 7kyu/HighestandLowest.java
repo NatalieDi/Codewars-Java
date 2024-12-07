397f51004097413081d5523c2ade7b58ac07deee
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-07 00:49:16 -0600

    updated some tasks to streams

--- 7kyu/HighestandLowest.java
+++ 7kyu/HighestandLowest.java
@@ -7,30 +7,45 @@ highAndLow("1 2 3 4 5")  // return "5 1"
 highAndLow("1 2 -3 4 5") // return "5 -3"
 highAndLow("1 9 3 4 -5") // return "9 -5"
  */
+
+import java.util.Arrays;
+
 public class HighestandLowest {
     public static void main(String[] args) {
         System.out.println(highAndLow("1 2 3 4 5"));// "5 1"
         System.out.println(highAndLow("1 2 -3 4 5"));// "5 -3"
         System.out.println(highAndLow("1 9 3 4 -5"));// "9 -5"
     }
+//    public static String highAndLow(String numbers) {
+//        String[] numbersArray = numbers.split(" ");
+//        String max = numbersArray[0];
+//        String min = numbersArray[0];
+//
+//        for (int i = 0; i < numbersArray.length; i++) {
+//            if (Integer.parseInt(max) <= Integer.parseInt(numbersArray[i])) {
+//                max = numbersArray[i];
+//
+//            }
+//            if (Integer.parseInt(min) >= Integer.parseInt(numbersArray[i])) {
+//                min = numbersArray[i];
+//
+//
+//            }
+//        }
+//        return (max + " " + min);
+//
+//    }
+
+
     public static String highAndLow(String numbers) {
-        String[] numbersArray = numbers.split(" ");
-        String max = numbersArray[0];
-        String min = numbersArray[0];
+        int[] mas = Arrays.stream(numbers.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int max = Arrays.stream(mas).max().orElseThrow();
+        int min = Arrays.stream(mas).min().orElseThrow();
 
-        for (int i = 0; i < numbersArray.length; i++) {
-            if (Integer.parseInt(max) <= Integer.parseInt(numbersArray[i])) {
-                max = numbersArray[i];
-
-            }
-            if (Integer.parseInt(min) >= Integer.parseInt(numbersArray[i])) {
-                min = numbersArray[i];
-
-
-            }
-        }
-        return (max + " " + min);
-
+        return max + " " + min;
     }
-
 }
+
+
--- 8kyu/Abbreviate_a_two_word_name.java
+++ 8kyu/Abbreviate_a_two_word_name.java
@@ -6,13 +6,25 @@
 // patrick feeney => P.F
 //STRINGS FUNDAMENTALS
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Abbreviate_a_two_word_name {
     public static void main(String[] args) {
         System.out.println(abbrevName("Natalie Diekmann"));
     }
+//    public static String abbrevName(String name) {
+//        name = name.toUpperCase();
+//        String[] names = name.split(" ");
+//        return names[0].charAt(0)+"."+names[1].charAt(0);
+//      }
+
+
     public static String abbrevName(String name) {
-        name = name.toUpperCase();    
-        String[] names = name.split(" ");    
-        return names[0].charAt(0)+"."+names[1].charAt(0);
-      }
+        return Arrays.stream(name.split(" "))
+                .map(word -> word.substring(0, 1).toUpperCase())
+                .collect(Collectors.joining("."));
+
+    }
+
 }
